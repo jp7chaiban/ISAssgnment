888dae3254d5ae00a409fcf7cba4b757c9707e6a
pbeaini2 <65338080+pbeaini2@users.noreply.github.com>
2020-10-22 01:01:38 +0300

    Update ImmuneFlask.py

--- BlueTeam/ImmuneFlask.py
+++ BlueTeam/ImmuneFlask.py
@@ -1,9 +1,9 @@
 # Same Flask App modified to handle DOS
 import flask
-
+from scapy.all import Ether, ARP, srp, sniff, conf
 import struct
-import request
-dict = {}
+# import request
+nest_dict = {}
 from datetime import datetime
 
 # file_txt = open("attack_DDoS.txt", 'a')
@@ -12,25 +12,35 @@ app = flask.Flask(__name__)
 
 @app.before_request
 def block_method():
-    t1 = str(datetime.now())
     # ip_address = request.environ.get('REMOTE_ADDR')
     ip_address = flask.request.remote_addr
+    t1 = str(datetime.microsecond)
     print("Request from: ", ip_address)
-    print("Current Dict: ", dict)
-    if ip_address in dict:
-        dict[ip_address] = dict[ip_address] + 1
-        print(dict[ip_address])
-        if dict[ip_address] > threshold:  # and (dict[ip_address] < threshold + 10):
-
-            line = "DDOS attack is Detected from " + ip_address + " with " + str(dict[ip_address]) + " requests. \nBlocking access !"
+    print("Current Dict: ", nest_dict)
+    print("Time of Request: ", t1)
+    if ip_address in nest_dict:
+        nest_dict[ip_address]['requests'] = nest_dict[ip_address]['requests'] + 1
+        print("Current Dict: ", nest_dict[ip_address]['requests'])
+        nest_dict[ip_address]['time'] = t1
+        print(nest_dict[ip_address]['requests'])
+     if nest_dict[ip_address]['time'] > 100:
+        if nest_dict[ip_address]['requests'] > threshold:  # and (nest_dict[ip_address] < threshold + 10):
+            line = "DDOS attack is Detected from " + ip_address + " with " + str(nest_dict[ip_address]['requests']) + " requests. \nBlocking access !"
             print(line)
             '''file_txt.writelines(line)
             file_txt.writelines(ip_address)
             file_txt.writelines("\n")'''
             flask.abort(403)
     else:
-        dict[ip_address] = 1
-        print("Updated Dict: ", dict)
+        line = "DDOS attack is Detected from " + ip_address
+        print(line)
+        '''file_txt.writelines(line)
+        file_txt.writelines(ip_address)
+        file_txt.writelines("\n")'''
+        flask.abort(403)
+    else:
+    nest_dict[ip_address]['requests'] = 1
+    print("Updated Dict: ", dict)
 
 
 @app.route('/')
